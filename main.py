c0797d886630437f1f3a7b290255ef21665b34a9
Ranjan098 <144373098+Ranjan098@users.noreply.github.com>
2023-11-12 02:40:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ import re
 import os
 
 bot = Client("bot",
-             bot_token= "6274323365:AAEYfxDdbLS0IYjrEDuplz2m0MMmkSwZn40",
+             bot_token= "6787006839:AAHVTEcSB4F1DhZUDt4WWsODR2PXpAJT8Ts",
              api_id=   12606917,
              api_hash= "f25113b8c17dca6fa7abda53a86bd4f7")
 
